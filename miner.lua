f581467925f0bc054312649a355bad9d2651a9dc
Kristopher38 <kristopher38@wp.pl>
2020-01-04 18:37:25 +0100

    Reworked mining algorithm, now uses latest improvements in navigation class and can actually mine an ore lump (only with perfect map info) with a kind of greedy traveling salesman problem algorithm

--- miner.lua
+++ miner.lua
@@ -1,75 +1,75 @@
 local component = require("component")
 local sides = require("sides")
+local robot = require("robot")
 local geolyzer = component.geolyzer
+local glasses = component.glasses
+
 local utils = require("utils")
 local map = require("map")
+local nav = require("navigation")
+local vec3 = require("vec3")
+
+glasses.startLinking("Kristopher38")
+glasses.setRenderPosition("absolute")
+local lookingAt = glasses.getUserLookingAt("Kristopher38")
+robot.position = vec3(lookingAt.x, lookingAt.y, lookingAt.z)
+robot.orientation = sides.west -- nav.detectOrientation()
+glasses.removeAll()
+print("Mapping...")
+map.scanMap(9, 9, 9)
+print("Mapped count", map.count)
+print("Finished mapping sorrounding area")
 
 --[[ blocks (vectors) to be inspected, either by comparing to currently 
 mined ore or analyzed by the geolyzer for higher operation speed --]]
-local toCheckVectors = {}
-local toMineVectors = {} -- blocks (vectors) to be mined
-local robotFacing = sides.front -- robot's absolute map orientation (assume front = east)
-local robotPosition = vec3(0, 70, 0) -- robot's absolute map position 
+local toMineVectors = {} -- blocks to be mined
 
--- modifies global state
-local function addOffsetToCheck(offsetVec)
-	if not hasDuplicateValue(toCheckVectors, vector) then
-		table.insert(toCheckVectors, coordsFromOffset(robotPosition, offsetVec, robotFacing))
+local function pickNextTargetPath()
+	local bestPath
+	local bestCost = math.huge
+	local bestPos
+	for i, target in ipairs(toMineVectors) do
+		local path, cost = nav.aStar(robot.position, robot.orientation, target)
+		if cost < bestCost then
+			bestCost = cost
+			bestPath = path
+			bestPos = i
+		end
+	end
+	if bestPath ~= nil then
+		table.remove(toMineVectors, bestPos)
+		return bestPath
+	else
+		error("Couldn't find best target")
 	end
 end
 
--- modifies global state
-local function addOffsetToMine(offsetVec)
-	if not hasDuplicateValue(toMineVectors, vector) then
-		table.insert(toMineVectors, coordsFromOffset(robotPosition, offsetVec, robotFacing))
+local function mineOreLump()
+	table.insert(toMineVectors, nav.coordsFromOffset(robot.position, vec3(1, 0, 0), robot.orientation))
+
+	while #toMineVectors > 0 do
+		local nextTargetPath = pickNextTargetPath()
+		local target = table.remove(nextTargetPath, 1)
+		nav.navigatePath(nextTargetPath)
+		nav.faceBlock(target)
+		local deltaY = robot.position.y - target.y
+		if deltaY == 0 then
+			robot.swing()
+		elseif deltaY == -1 then
+			robot.swingUp()
+		else
+			robot.swingDown()
+		end
+		map[target] = "minecraft:air"
+
+		for i, vector in ipairs(nav.neighbours(target)) do
+			if map[vector] == "minecraft:ore" then
+				if not utils.hasDuplicateValue(toMineVectors, vector) then
+					table.insert(toMineVectors, vector)
+				end
+			end
+		end
 	end
 end
 
-local function pickNextTarget(toCheck, toMine)
-	return nil
-end
-
--- modifies global state
-local function mineOreLump(oreName)
-	local lumpDepleted = false
-	
-	while not lumpDelpeted do
-		robot.swing()
-		addOffsetToCheck(vec3(2, 0, 0)) -- block two blocks in front
-		addOffsetToCheck(vec3(1, 1, 0)) -- block front-up
-		addOffsetToCheck(vec3(1, -1, 0)) -- block front-down
-		if robot.compareUp() then
-			addOffsetToMine(vec3(0, 1, 0)) -- block above
-			-- robot.swingUp()
-			-- addOffsetToCheck({0, 2, 0}) -- block above
-			-- addOffsetToCheck({-1, 1, 0}) -- block above-behind
-			-- no need to add block above-front because it was added as block front-up before
-		end
-		if robot.compareDown() then
-			addOffsetToMine(vec3(0, -1, 0)) -- block below
-			-- robot.swingDown()
-			-- addOffsetToCheck({0, -2, 0}) -- block below
-			-- addOffsetToCheck({-1, -1, 0}) -- block below-behind
-			-- no need to add block below-front because it was added as block front-down before
-		end
-		if geolyzer.analyze(sides.left)["name"] == oreName then
-			addOffsetToMine(vec3(0, 0, -1)) -- block to the left
-		end
-		if geolyzer.analyze(sides.right)["name"] == oreName then
-			addOffsetToMine(vec3(0, 0, 1)) -- block to the right
-		end
-		
-		local nextTarget = pickNextTarget(toCheckVectors, toMineVectors)
-		-- 
-	end
-end
-
-map[robotPosition] = "minecraft:air"
-mineOreLump("minecraft:iron_ore")
-
---[[ Coord system
-# MC logic
-Coords... North: z--, East: x++, South: z++, West: x--, Up: y++, Down: y--
-Coords... X points East, Z points South, Y points Up
-Facing... 0: South, 1: West, 2: North, 3: East
-Facing... Starts at South, goes clockwise --]]
+mineOreLump()
