4bf8c6b35413fa9b3c69013d0dd45f20be2364f1
Kristopher38 <kristopher38@wp.pl>
2020-01-04 18:30:10 +0100

    Added utility function to print vec3 objects as strings

--- vec3.lua
+++ vec3.lua
@@ -2,7 +2,12 @@ local function vec3eq(a, b)
 	return a.x == b.x and a.y == b.y and a.z == b.z
 end
 
-local mt = {__eq = vec3eq}
+local function vec3tostring(a)
+	return "[" .. a.x .. ", " .. a.y .. ", " .. a.z .. "]"
+end
+
+local mt = {__eq = vec3eq,
+			__tostring = vec3tostring}
 
 -- constructs vector as a table with "x", "y", "z" keys from table of 3 coordinates or 3 coordinate arguments
 local function vec3(_x, _y, _z)
