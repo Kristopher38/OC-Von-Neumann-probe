615b70675820fd1a42db18ff48914a63a8ee9a62
Kristopher38 <kristopher38@wp.pl>
2020-01-14 20:40:22 +0100

    Added blockType enum to make working with blocks with specific hardness easier and more standardized

---
+++ blocktype.lua
@@ -0,0 +1,14 @@
+local blockType = {
+    air = 0,
+    leaves = 0.2,
+    dirt = 0.5,
+    stone = 1.5,
+    wood = 2,
+    chest = 2.5,
+    ore = 3,
+    obsidian = 50,
+    fluid = 100,
+    unknown = -1
+}
+
+return blockType
