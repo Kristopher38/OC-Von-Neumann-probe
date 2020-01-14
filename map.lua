d73ce6eebd78ee4cd3e2495a84e3b3b18cac2f9b
Kristopher38 <kristopher38@wp.pl>
2020-01-14 20:42:02 +0100

    Moved scanning functions to another module which now take form of batches, which can later be queried for a specific block category

--- map.lua
+++ map.lua
@@ -4,53 +4,19 @@ local geolyzer = component.geolyzer
 
 local vec3 = require("vec3")
 local VectorMap = require("vectormap")
+local blockType = require("blocktype")
 
 local map = VectorMap(vec3(256, 128, 256))
 
-function map.assumeBlock(hardness)
-	local assumedBlock = "minecraft:air"
-	if hardness < 0.4 then assumedBlock = "minecraft:air"
-	elseif hardness < 2.75 then assumedBlock = "minecraft:stone"
-	elseif hardness < 95 then assumedBlock = "minecraft:ore"
-	else assumedBlock = "minecraft:water" end
-	return assumedBlock
-end
-
--- scan the sorrounding sizex x sizey x sizez area with robot roughly in the center
-function map.scanMap(sizex, sizey, sizez)
-    local startx = -math.floor(sizex / 2)
-    local endx = math.floor(sizex / 2) - (sizex % 2 == 0 and 1 or 0)
-    local starty = -math.floor(sizey / 2)
-    --local endy = math.floor(sizey / 2) - (sizey % 2 == 0 and 1 or 0)
-    local startz = -math.floor(sizez / 2)
-    local endz = math.floor(sizez / 2) - (sizez % 2 == 0 and 1 or 0)
-	for x = startx, endx, 1 do
-		for z = startz, endz, 1 do
-            local scanData = geolyzer.scan(x, z, starty, 1, 1, sizey)
-            for i = 1, sizez do
-				map[vec3(robot.position.x + x, robot.position.y + i + starty - 1, robot.position.z + z)] = map.assumeBlock(scanData[i])
-       		end
-		end
-	end
-end
-
---[[ scan the provided quad, note that w, d and h should be positive nonzero because of
-readability (the scan will still work but you may get the results different than expected,
-e.g. when providing w = -2 you'll get result as if length of width was 4
-Also note that geolyzer scans orientation are absolute, regardless of robot's current orientation
-and this function as of now doesn't correct for this. The linear table returned from geolyzer.scan
-should be interpreted as: first, values go towards positive x, then towards positive z, then
-towards positive y --]]
-function map.scan(x, z, y, w, d, h)
-	local scanData = geolyzer.scan(x, z, y, w, d, h)
-	local i = 1
-	for yy = 0, h - 1 do
-		for zz = 0, d - 1 do
-			for xx = 0, w - 1 do
-				map[vec3(robot.position.x + x + xx, robot.position.y + y + yy, robot.position.z + z + zz)] = map.assumeBlock(scanData[i])
-				i = i + 1
-			end
-		end
+function map.assumeBlockType(hardness)
+	if hardness ~= nil then
+		if hardness < 0.4 then assumedType = blockType.air
+		elseif hardness < 2.75 then assumedType = blockType.stone
+		elseif hardness < 95 then assumedType = blockType.ore
+		else assumedType = blockType.fluid end
+		return assumedType
+	else
+		return blockType.unknown
 	end
 end
 
---
+++ scanbatch.lua
@@ -0,0 +1,78 @@
+local component = require("component")
+local geolyzer = component.geolyzer
+local robot = require("robot")
+
+local map = require("map")
+local vec3 = require("vec3")
+local blockType = require("blockType")
+
+local ScanBatch = {}
+ScanBatch.__index = ScanBatch
+setmetatable(ScanBatch, {__call = function(cls)
+	local self = {}
+	self.scans = {}
+	setmetatable(self, cls) -- cls = PriorityQueue
+	return self
+end })
+
+--[[ scan the sorrounding sizex x sizey x sizez area with robot roughly in the center
+TODO: implement cube packing algorithm --]]
+function ScanBatch:scanAround(sizeVector, centerVector)
+    local startx = -math.floor(sizeVector.x / 2)
+    local endx = math.floor(sizeVector.x / 2) - (sizeVector.x % 2 == 0 and 1 or 0)
+    local starty = -math.floor(sizeVector.y / 2)
+    local startz = -math.floor(sizeVector.z / 2)
+    local endz = math.floor(sizeVector.z / 2) - (sizeVector.z % 2 == 0 and 1 or 0)
+    for x = startx, endx, 1 do
+        for z = startz, endz, 1 do
+            local scanData = geolyzer.scan(x, z, starty, 1, 1, sizeVector.y)
+            for i = 1, sizez do
+                map[centerVector + vec3(x, i + starty - 1, z)] = scanData[i]
+            end
+        end
+    end
+end
+
+--[[ scan the provided quad, note that w, d and h should be positive nonzero because of
+readability (the scan will still work but you may get the results different than expected,
+e.g. when providing w = -2 you'll get result as if length of width was 4
+Also note that geolyzer scans orientation are absolute, regardless of robot's current orientation
+and this function as of now doesn't correct for this (TODO: correct this with coordsFromOffset). 
+The linear table returned from geolyzer.scan should be interpreted as: first, values go towards
+positive x, then towards positive z, then towards positive y --]]
+function ScanBatch:scanQuad(offsetVector, sizeVector)
+	local scanData = geolyzer.scan(offsetVector.x, offsetVector.z, offsetVector.y, sizeVector.x, sizeVector.z, sizeVector.y)
+	local i = 1
+	for y = 0, sizeVector.y - 1 do
+		for z = 0, sizeVector.z - 1 do
+            for x = 0, sizeVector.x - 1 do
+				map[robot.position + offsetVector + vec3(x, y, z)] = scanData[i]
+				i = i + 1
+			end
+		end
+	end
+	table.insert(self.scans, {robot.position + offsetVector, sizeVector})
+end
+    
+function ScanBatch:query(block)
+    local results = {}
+    for i, scan in ipairs(self.scans) do
+        local offsetVector = scan[1]
+        local sizeVector = scan[2]
+        for x = 0, sizeVector.x - 1 do
+            for y = 0, sizeVector.y - 1 do
+                for z = 0, sizeVector.z - 1 do
+                    local blockVector = vec3(offsetVector.x + x, offsetVector.y + y, offsetVector.z + z)
+                    local block = map[blockVector]
+                    --if block >= min and block <= max then
+                    if block == blockType.ore then
+                        table.insert(results, blockVector)
+                    end
+                end
+            end
+        end
+    end
+    return results
+end
+
+return ScanBatch
