5405d76acf8884485820a10e69328fa5746776aa
Kristopher38 <kristopher38@wp.pl>
2020-01-14 20:45:03 +0100

    Updated vectormap tests to accommodate for the changes in block type querying from map

--- vectormap_tests.lua
+++ vectormap_tests.lua
@@ -1,10 +1,11 @@
 local VectorMap = require("vectormap")
 local vec3 = require("vec3")
+local blockType = require("blocktype")
 
 local function testIndexNewIndex()
 	local map = VectorMap()
-	map[vec3(7, 1, 3)] = "minecraft:air"
-	local testGet = map[vec3(7, 1, 3)] == "minecraft:air"
+	map[vec3(7, 1, 3)] = blockType.air
+	local testGet = map[vec3(7, 1, 3)] == blockType.air
 	local testNotFound = map[vec3(3, 7, 5)] == nil
 	return testGet and testNotFound
 end
