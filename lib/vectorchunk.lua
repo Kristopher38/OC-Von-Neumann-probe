431ef0222d0c601c95c073c4bcf3416e8d32ea82
Kristopher38 <kristopher38@wp.pl>
2020-02-22 23:37:48 +0100

    Modified vectorchunk to take offset and store vectors based on it to make it possible to use it aStar

--- lib/vectorchunk.lua
+++ lib/vectorchunk.lua
@@ -3,8 +3,11 @@ local utils = require("utils")
 
 local VectorChunk = {}
 VectorChunk.__index = VectorChunk
-setmetatable(VectorChunk, {__call = function(cls)
+setmetatable(VectorChunk, {__call = function(cls, offset, centerOffset, packValues)
     local self = {}
+    self.data = {}
+    self.offset = centerOffset and offset - vec3(2048, offset.y, 2048) or (offset or vec3(0, 0, 0))
+    self.packValues = packValues or false
 
     setmetatable(self, cls)
     return self
@@ -12,53 +15,57 @@ end })
 
 --[[ Packs x, y, z values in range x: [0, 4095], y: [0, 255], z: [0, 4095]
 into a single integer. The packed format is [x: 12 bits, y: 8 bits, z: 12 bits] --]]
-local function packxyz(x, y, z)
-    return ((x & 0x00000FFF) << 20) | ((y & 0x000000FF) << 12) | (z & 0x00000FFF)
+local function packxyz(x, y, z, offset)
+    return (((x - offset.x) & 0x00000FFF) << 20) | (((y - offset.y) & 0x000000FF) << 12) | ((z - offset.z) & 0x00000FFF)
 end
 
 --- unpacks vector as a single integer into x, y, z values
-local function unpackxyz(number)
-    return (number & 0xFFF00000) >> 20, (number & 0x000FF000) >> 12, number & 0x00000FFF
+local function unpackxyz(number, offset)
+    return ((number & 0xFFF00000) >> 20) + offset.x, ((number & 0x000FF000) >> 12) + offset.y, (number & 0x00000FFF) + offset.z
 end
 
-function VectorChunk:at(vector)
-    return rawget(self, packxyz(vector.x, vector.y, vector.z))
+-- all that verbosity below is for extra performance
+
+function VectorChunk:at(vec)
+    local value = rawget(self.data, packxyz(vec.x, vec.y, vec.z, self.offset))
+    return self.packValues and (value and vec3(unpackxyz(value, self.offset)) or nil) or value
 end
 
 function VectorChunk:atxyz(x, y, z)
-    return rawget(self, packxyz(x, y, z))
+    local value = rawget(self.data, packxyz(x, y, z, self.offset))
+    return self.packValues and (value and vec3(unpackxyz(value, self.offset)) or nil) or value
 end
 
-function VectorChunk:set(vector, element)
-    rawset(self, packxyz(vector.x, vector.y, vector.z), element)
+function VectorChunk:set(vec, elem)
+    rawset(self.data, packxyz(vec.x, vec.y, vec.z, self.offset), self.packValues and packxyz(elem.x, elem.y, elem.z, self.offset) or elem)
 end
 
-function VectorChunk:setxyz(x, y, z, element)
-    rawset(self, packxyz(x, y, z), element)
+function VectorChunk:setxyz(x, y, z, elem)
+    rawset(self.data, packxyz(x, y, z, self.offset), self.packValues and packxyz(elem.x, elem.y, elem.z, self.offset) or elem)
 end
 
-function VectorChunk.__index(self, vector)
-    if utils.isInstance(vector, vec3) then
-        return self:at(vector)
+function VectorChunk.__index(self, vec)
+    if utils.isInstance(vec, vec3) then
+        return self:at(vec)
     else
-        return getmetatable(self)[vector] -- gets the metatable with methods and metamethods
+        return getmetatable(self)[vec] -- gets the metatable with methods and metamethods
     end
 end
 
-function VectorChunk.__newindex(self, vector, element)
-    if utils.isInstance(vector, vec3) then
-        self:set(vector, element)
+function VectorChunk.__newindex(self, vec, elem)
+    if utils.isInstance(vec, vec3) then
+        self:set(vec, elem)
     else
-        rawset(self, vector, element)
+        rawset(self, vec, elem)
     end
 end
 
 function VectorChunk.__pairs(self)
     local function statelessIterator(self, index)
         local element
-        index, element = next(self, index and packxyz(index.x, index.y, index.z) or index)
+        index, element = next(self.data, index and packxyz(index.x, index.y, index.z, self.offset) or index)
         if element then
-            return vec3(unpackxyz(index)), element
+            return vec3(unpackxyz(index, self.offset)), self.packValues and vec3(unpackxyz(element, self.offset)) or element
         end
     end
 
