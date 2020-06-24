42351d01a6070af1c1988a52bebd2f084cb4c938
Kristopher38 <kristopher38@wp.pl>
2020-06-24 14:43:08 +0200

    Added cube packing algorithm to ScanBatch. Scanning now takes into account robot's orientation. Closes #43

--- lib/scanbatch.lua
+++ lib/scanbatch.lua
@@ -8,6 +8,7 @@ local blockType = require("blockType")
 local VectorChunk = require("vectorchunk")
 local VectorMap = require("vectormap")
 local utils = require("utils")
+local sides = require("sides")
 
 local debug = require("debug")
 
@@ -20,35 +21,84 @@ setmetatable(ScanBatch, {__call = function(cls)
 	return self
 end })
 
---[[ scan the sorrounding sizex x sizey x sizez area with robot roughly in the center
-TODO: implement cube packing algorithm --]]
-function ScanBatch:scanAround(sizeVector)
-    local startx = -math.floor(sizeVector.x / 2)
-    local endx = math.floor(sizeVector.x / 2) - (sizeVector.x % 2 == 0 and 1 or 0)
-    local starty = -math.floor(sizeVector.y / 2)
-    local startz = -math.floor(sizeVector.z / 2)
-    local endz = math.floor(sizeVector.z / 2) - (sizeVector.z % 2 == 0 and 1 or 0)
-    for x = startx, endx, 1 do
-        for z = startz, endz, 1 do
-            local scanData = geolyzer.scan(x, z, starty, 1, 1, sizeVector.y)
-            for i = 1, sizeVector.z do
-                map[robot.position + vec3(x, i + starty - 1, z)] = scanData[i]
+--[[ finds a vector which x, y, z components define dimensions of a cuboid which is (almost) the most optimal one
+to fill another larger cuboid sizeVector with cuboids of that size, with algorithm described here:
+https://stackoverflow.com/questions/37644269/dividing-a-cuboid-of-n-into-smaller-cuboids-of-m-volume --]]
+function ScanBatch:packCubes(sizeVector)
+    local minCubes = math.ceil(sizeVector.x / 4) * math.ceil(sizeVector.y / 4) * math.ceil(sizeVector.z / 4)
+    local solution = vec3(4, 4, 4)
+    for x = 1, math.min(sizeVector.x, 64) do
+        for y = 1, math.min(sizeVector.y, 64 // x) do
+            local z = 64 // (x * y)
+            local numCubes = math.ceil(sizeVector.x / x) * math.ceil(sizeVector.y / y) * math.ceil(sizeVector.z / z)
+            if numCubes < minCubes then
+                minCubes = numCubes
+                solution.x = x
+                solution.y = y
+                solution.z = z
             end
         end
     end
-    self.scans[robot.position + vec3(startx, starty, startz)] = sizeVector
+
+    return solution, minCubes
 end
 
---[[ scan the provided quad, note that w, d and h should be positive nonzero because of
-readability (the scan will still work but you may get the results different than expected,
-e.g. when providing w = -2 you'll get result as if length of width was 4
-Also note that geolyzer scans orientation are absolute, regardless of robot's current orientation
-and this function as of now doesn't correct for this (TODO: correct this with coordsFromOffset). 
+--[[ scan an area of any size, volumes larger than 64 are handled by dividing the space into the least possible
+amount of cuboids that fill the entire space to minimize delay and energy consumption ]]
+function ScanBatch:scanArea(offsetVector, sizeVector)
+    local scanSize = self:packCubes(sizeVector)
+
+    for x = 0, math.ceil(sizeVector.x / scanSize.x) - 1 do
+        for y = 0, math.ceil(sizeVector.y / scanSize.y) - 1 do
+            for z = 0, math.ceil(sizeVector.z / scanSize.z) - 1 do
+                local iterationOffset = vec3(x * (scanSize.x), y * (scanSize.y), z * (scanSize.z))
+                local overflow = iterationOffset + scanSize - sizeVector
+                overflow.x = math.max(0, overflow.x)
+                overflow.y = math.max(0, overflow.y)
+                overflow.z = math.max(0, overflow.z)
+                self:scanQuad(offsetVector + iterationOffset, scanSize - overflow)
+            end
+        end
+    end
+end
+
+--[[ scan an area around the robot, with robot roughly in the center --]]
+function ScanBatch:scanAround(sizeVector)
+    local offsetVector = vec3(-sizeVector.x // 2, -sizeVector.y // 2, -sizeVector.z // 2)
+    self:scanArea(offsetVector, sizeVector)
+end
+
+--[[ scan a 16x16 area around the robot, with robot roughly in the center and an optional vertical offset --]]
+function ScanBatch:scanLayer(offsetY)
+    offsetY = offsetY or 0
+    self:scanArea(vec3(-8, offsetY, -8), vec3(16, 1, 16))
+end
+
+--[[ takes the offset and size vectors and rotates them so that the origin point and dimensions for
+geolyzer.scan are relative to the robot's orientation --]]
+function ScanBatch:correctForOrientation(offsetVector, sizeVector)
+    if robot.orientation == sides.east then
+        return offsetVector, sizeVector
+    elseif robot.orientation == sides.west then
+        return vec3(-offsetVector.x - sizeVector.x + 1, offsetVector.y, -offsetVector.z - sizeVector.z + 1), sizeVector
+    elseif robot.orientation == sides.south then
+        return vec3(-offsetVector.z - sizeVector.z + 1, offsetVector.y, offsetVector.x), vec3(sizeVector.z, sizeVector.y, sizeVector.x)
+    else -- robot.orientation == sides.north
+        return vec3(offsetVector.z, offsetVector.y, -offsetVector.x - sizeVector.x + 1), vec3(sizeVector.z, sizeVector.y, sizeVector.x)
+    end
+end
+
+--[[ scan the provided quad, note that x, y and z of sizeVector should all be non-negative values
+for readability and to avoid unexpected behavior (the scan will still work but you may get different
+results than expected, e.g. when providing x = -2 you'll get result as if x was 4
+offsetVector takes into account robot's orientation, in other words offset is relative to where
+the robot is currently looking (x offset means forward/backward, y means up/down and z means left/right)
 The linear table returned from geolyzer.scan should be interpreted as: first, values go towards
 positive x, then towards positive z, then towards positive y --]]
 function ScanBatch:scanQuad(offsetVector, sizeVector)
+    offsetVector, sizeVector = self:correctForOrientation(offsetVector, sizeVector) -- correct for robot's orientation
 	local scanData = geolyzer.scan(offsetVector.x, offsetVector.z, offsetVector.y, sizeVector.x, sizeVector.z, sizeVector.y)
-	local i = 1
+    local i = 1
 	for y = 0, sizeVector.y - 1 do
 		for z = 0, sizeVector.z - 1 do
             for x = 0, sizeVector.x - 1 do
@@ -59,20 +109,9 @@ function ScanBatch:scanQuad(offsetVector, sizeVector)
     end
     self.scans[robot.position + offsetVector] = sizeVector
 end
-    
-function ScanBatch:scanLayer(offsetY)
-    offsetY = offsetY or 0
-    local quads = {
-		{vec3(0, offsetY, -8), vec3(8, 1, 8)},
-		{vec3(0, offsetY, 0), vec3(8, 1, 8)},
-		{vec3(-8, offsetY, -8), vec3(8, 1, 8)},
-		{vec3(-8, offsetY, 0), vec3(8, 1, 8)}
-	}
-	for i, quad in ipairs(quads) do
-		self:scanQuad(table.unpack(quad))
-	end
-end
 
+--[[ make a query for a list of coordinates containing a specific block type or specific block types,
+returns a table of vec3 values which are absolute coordinates where the specific block types were detected ]]
 function ScanBatch:query(_blockType)
     if type(_blockType) ~= "table" then
         _blockType = {_blockType}
@@ -94,6 +133,7 @@ function ScanBatch:query(_blockType)
     return results
 end
 
+-- same as ScanBatch:query but returns an iterator instead of a table
 function ScanBatch:queryPairs(_blockType)
     if type(_blockType) ~= "table" then
         _blockType = {_blockType}
