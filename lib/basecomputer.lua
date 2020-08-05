14d92bdf51dde9438de3e19958a8ae0a2207dcf2
Kristopher38 <kristopher38@wp.pl>
2020-08-05 23:46:38 +0200

    Started work on base's computer interaction module and charger module

---
+++ lib/basecomputer.lua
@@ -0,0 +1,90 @@
+local utils = require("utils")
+local event = require("event")
+local component = require("component")
+local modem = component.modem
+
+if not modem.isWireless() then
+    error("Robot requires wireless network card to function properly")
+end
+
+local BaseComputer = utils.makeClass(function()
+    local self = {}
+    self.modemAddr = nil
+    self.commsPort = 1957 -- year of von Neumann's death
+    self.cmdDelay = 0.5 -- wait 10 ticks before assuming the packet got lost
+    self.wakeCmd = nil
+    self.wakeDelay = 6.0  -- OpenOS boot time on tier 1 hardware
+    self.pingCmd = "ping"
+    self.assemblerStartCmd = "assemblerStart"
+    self.assemblerStatusCmd = "assemblerStatus"
+
+    return self
+end)
+
+function BaseComputer:assurePortOpen()
+    if not modem.isOpen(self.commsPort) then
+        modem.open(self.commsPort)
+    end
+end
+
+function BaseComputer:sendCmd(command, delay, retries, broadcast)
+    retries = retries or 1
+    broadcast = broadcast or false
+
+    self:assurePortOpen()
+    for i = 1, retries do
+        if broadcast then
+            modem.broadcast(self.commsPort, command)
+        else
+            modem.send(self.modemAddr, self.commsPort, command)
+        end
+        local response = table.pack(event.pull(delay, "modem_message"))
+        if response.n > 0 then
+            return response
+        else
+            return nil
+        end
+    end
+end
+
+function BaseComputer:wake(retries)
+    retries = retries or 1
+    for i = 1, retries do
+        self:sendCmd(self.wakeCmd, self.wakeDelay, 1, true)
+        if self:ping() then
+            return true
+        end
+    end
+    return false
+end
+
+function BaseComputer:ping(retries)
+    local response = self:sendCmd(self.pingCmd, self.cmdDelay, retries, false)
+    return response ~= nil
+end
+
+function BaseComputer:configure()
+
+end
+
+function BaseComputer:assemblerStart(retries)
+    local response = self:sendCmd(self.assemblerStartCmd, self.cmdDelay, retries, false)
+    
+end
+
+function BaseComputer:assemblerStatus(retries)
+    local response = self:sendCmd(self.assemblerStatusCmd, self.cmdDelay, retries, false)
+end
+
+function BaseComputer:shutdown(retries)
+    retries = retries or 1
+    for i = 1, retries do
+        self:sendCmd(self.shutdownCmd, self.cmdDelay, 1, false)
+        if not self:ping() then
+            return true
+        end
+    end
+    return false
+end
+
+return BaseComputer
---
+++ lib/charger.lua
@@ -0,0 +1,41 @@
+local utils = require("utils")
+local blacklistMap = require("blacklistmap")
+local navigation = require("navigation")
+local vec3 = require("vec3")
+local robot = require("robot")
+
+local Charger = utils.makeClass(function(position)
+    local self = {}
+    local position = position
+
+    blacklistMap[position] = true
+    return self
+end)
+
+function Charger:goTo()
+    navigation.goTo(navigation.coordsFromOffset(self.position, vec3(0, 1, 0)))
+end
+
+function Charger:isOn()
+    if robot.position == navigation.coordFromOffset(self.position, vec3(0, 1, 0)) then
+        local energy = computer.energy()
+        os.sleep(0.5)
+        return computer.energy() - energy > 0
+    else
+        error("Robot is not above the charger")
+    end
+end
+
+function Charger:charge(targetCharge, eps)
+    if self:isOn() then
+        targetCharge = targetCharge or 1.0
+        eps = eps or 0.001
+        local currentCharge = computer.energy() / computer.maxEnergy()
+        while currentCharge < targetCharge - eps do
+            os.sleep(0.5)
+            currentCharge = computer.energy() / computer.maxEnergy()
+        end
+    end
+end
+
+return Charger
