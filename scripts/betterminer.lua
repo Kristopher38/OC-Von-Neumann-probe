e12a7dd40ef8465eb1435b69f3d0f9d451e991c7
Kristopher38 <kristopher38@wp.pl>
2020-08-14 18:42:54 +0200

    Revert "Restructured folder structure"
    This reverts commit 145537b2fee2d5337f577ef9f357af417e0b7461.

--- scripts/betterminer.lua
+++ scripts/betterminer.lua
@@ -2,7 +2,6 @@ package.loaded.mining = nil
 local mining = require("mining")
 local debug = require("debug")
 local sides = require("sides")
-local locLogger = require("locationlogger")
 
 --debug.init()
 --debug.clearWidgets()
