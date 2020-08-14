145537b2fee2d5337f577ef9f357af417e0b7461
Kristopher38 <kristopher38@wp.pl>
2020-08-14 17:31:50 +0200

    Restructured folder structure

--- src/scripts/betterminer.lua
+++ src/scripts/betterminer.lua
@@ -2,6 +2,7 @@ package.loaded.mining = nil
 local mining = require("mining")
 local debug = require("debug")
 local sides = require("sides")
+local locLogger = require("locationlogger")
 
 --debug.init()
 --debug.clearWidgets()
